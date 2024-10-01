192b003f780cd4e0e513a5bc471790b1c829abd4
agabin <agabin@appdev.yearup.org>
2024-10-01 16:07:33 -0400

    Added Loaded option for the sandwich shop

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -4,54 +4,67 @@ import java.util.*;
 public class SandwichShop {
 
     public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
-    double regPrice = 5.45;
-    double lrgPrice = 8.95;
-    double discount = 0;
-    double price = 0;
+        double regPrice = 5.45;
+        double lrgPrice = 8.95;
+        double discount = 0;
+        double price = 0;
+        double regLoaded = 1.00;
+        double lrgLoaded = 1.75;
 
-//prompt user to select size option
-    System.out.println("Hi! Welcome to Sandwich Shop ");
-    System.out.println("Please make a selection: ");
-    System.out.println("1. Regular ($5.45)");
-    System.out.println("2. Large ($8.95");
-    int size = s.nextInt();
-    s.nextLine();
+        //prompt user to select size option
+        System.out.println("Hi! Welcome to Sandwich Shop ");
+        System.out.println("Please make a selection: ");
+        System.out.println("1. Regular ($5.45)");
+        System.out.println("2. Large ($8.95");
+        int size = s.nextInt();
+        System.out.println("------------------------");
+        s.nextLine();
 
+//prompt user to choose if they would like a loaded sandwich
+        System.out.println("Would you like a loaded sandwich?");
+        System.out.println("Yes");
+        System.out.println("No");
+        String loaded = s.nextLine();
 
 //prompt user to select their age
-    System.out.println("Please select age group: ");
-    System.out.println("1. Student (10% off) ");
-    System.out.println("2. Senior (20% off) ");
-    int age = s.nextInt();
-    s.nextLine();
+        System.out.println("Please select age group: ");
+        System.out.println("1. Student (10% off) ");
+        System.out.println("2. Senior (20% off) ");
+        int age = s.nextInt();
+        s.nextLine();
+        System.out.println("------------------------");
 
 //price based on size selection
-    if (size == 1) {
-        price = regPrice;
-    }
-    else if (size == 2){
-        price = lrgPrice;
-        System.out.println();
-    }
+        if (size == 1) {
+            price = regPrice;
+        } else if (size == 2) {
+            price = lrgPrice;
+        }
 //discounts
-    if (age == 1) {
-        discount = 0.10;
-    }
-    else if (age == 2) {
-        discount = 0.20;
-        System.out.println();
-    }
-//Calculating discounts
-    double discountTotal = price * discount;
-    double total = price - discountTotal;
+        if (age == 1) {
+            discount = 0.10;
+        } else if (age == 2) {
+            discount = 0.20;
+        }
+//Calculating loaded option
+        if (loaded.equalsIgnoreCase("Yes")){
+            if (size == 1){
+                price += regLoaded;
+        } else if (size == 2){
+                price += lrgLoaded;
+            }
 
-    System.out.println("Your total is $" + String.format("%.2f", total));
+        }
+//Calculating totals
+        double discountTotal = price * discount;
+        double total = price - discountTotal;
+
+        System.out.println("Your total is $" + String.format("%.2f", total));
 
 
 
-
-    }
-}
+            }
+        }
 
